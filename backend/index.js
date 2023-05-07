3ea214ac942a1c9a317a2ac1141c0625b395c6cf
Bhagyavan Vansh <vanshbhagyavan@gmail.com>
2023-05-07 18:52:50 +0530

    my first MERN stack project commit

---
+++ backend/index.js
@@ -0,0 +1,71 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors())
+
+mongoose.connect('mongodb://127.0.0.1:27017/LoginProject').then(()=>{
+    console.log("Mongo connected!");
+}).catch((err)=>{
+    console.log(err)
+})
+
+const userSchema = mongoose.Schema({
+    name:String,
+    email:String,
+    password: String
+})
+
+const User = new mongoose.model("User", userSchema)
+
+//routes
+
+
+app.post('/login', (req, res) => {
+    const {email, password} = req.body
+    User.findOne({email: email}).then((result) => {
+        if(result){
+            if(password === result.password){
+                res.send({message: "Login successfully", result: result})
+            } else {
+                res.send({message : "Incorrect Password"})
+            }
+        }else {
+            res.send({message: 'User not registered.'})
+        }
+    })
+   
+   
+})
+
+app.post('/register', (req, res) => {
+    const {name, email, password} = req.body
+
+    User.findOne({email: email}).then((result)=>{
+        if(result){
+            res.send({message : "user already registered"})
+        } else {
+            const user = new User({
+                name,
+                email,
+                password
+            })
+
+            user.save().then(() => {
+                res.send({message:"User successfuly registered."})
+            }).catch((err) => {
+                console.log(err);
+            })
+        }
+    })
+            
+    })
+    
+
+
+app.listen(8000, () => {
+    console.log("Listening at port 8000");
+})
